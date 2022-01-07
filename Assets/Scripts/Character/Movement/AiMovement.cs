70bf66377cc68c5984132dab94bb19257b42898f
Górka Mateusz <maatiug@gmail.com>
2022-01-07 13:30:25 +0100

    Save ai path proggres

--- Assets/Scripts/Character/Movement/AiMovement.cs
+++ Assets/Scripts/Character/Movement/AiMovement.cs
@@ -1,22 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class AiMovement : CharacterMovement
 {
+    private class AiPathSave : AttributedSaveSerializable
+    {
+        public int NextPoint;
+
+        public override string SubKey => "AiPath";
+        public AiPathSave(string parentKey) : base(parentKey) { }
+    }
+
     [SerializeField] private Transform[] _path;
     [SerializeField] const float _minDistance = 2f;
 
-    private int _nextPoint = -1;
+    private AiPathSave _value;
 
-    public Transform NextPoint => _path[_nextPoint];
+    public Transform NextPoint => _path[_value.NextPoint];
     public float DistanceToNext => Vector3.Distance(Character.Position, NextPoint.position);
 
     protected override void HandleInput()
     {
     }
 
+    protected override IEnumerator Start()
+    {
+        _value = new AiPathSave(Character.Id);
+        yield return SaveManager.Wait();
+        SaveManager.Instance.Load(ref _value);
+        SetDestination();
+    }
+
     protected override void AfterGameLoaded()
     {
-        GoNext();
     }
 
     protected override void Update()
@@ -28,12 +44,19 @@ public class AiMovement : CharacterMovement
     private void Movement()
     {
         if (DistanceToNext < _minDistance)
-            GoNext();
+        {
+            NextTarget();
+            SetDestination();
+        }
     }
 
-    private void GoNext()
+    private void SetDestination()
     {
-        _nextPoint = (_nextPoint + 1) % _path.Length;
         _agent.SetDestination(NextPoint.position);
+    }
+
+    private void NextTarget()
+    {
+        _value.NextPoint = (_value.NextPoint + 1) % _path.Length;
     }
 }
