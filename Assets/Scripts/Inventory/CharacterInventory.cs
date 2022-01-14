9cfeedae1e92f434b92c53423df2d8a654c09c8b
Górka Mateusz <maatiug@gmail.com>
2022-01-14 18:54:44 +0100

    CharacterInventory save base

--- Assets/Scripts/Inventory/CharacterInventory.cs
+++ Assets/Scripts/Inventory/CharacterInventory.cs
@@ -1,18 +1,39 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class CharacterInventory : MonoBehaviour
+public class CharacterInventory : Ability
 {
-    [SerializeField] private List<InventoryItem> _inventory = new List<InventoryItem>();
+    private class CharacterInventoryData : AttributedSaveSerializable
+    {
+        public override string SubKey => "inventory";
+        public CharacterInventoryData(string parentKey) : base(parentKey) { }
+
+        public void Add(InventoryItem item)
+        {
+            // todo
+        }
+    }
+
     [SerializeField] private CharacterColliderInteractions _interactions;
 
     private CharacterInventoryVisuals _visuals;
+    private CharacterInventoryData _inventory;
 
-    private void Awake()
+    protected void Awake()
     {
         _visuals = GetComponent<CharacterInventoryVisuals>();
         _interactions.AddListener(new ColliderListener<PickableContainer>(Collect, null));
+    }
+
+    protected IEnumerator Start()
+    {
+        _inventory = new CharacterInventoryData(Character.Id);
+        yield return SaveManager.Wait();
+        SaveManager.Instance.Load(ref _inventory);
     }
 
     public void Collect(PickableContainer item)
--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "InventoryItem", menuName = "ScriptableObjects")]
 public class InventoryItem : ScriptableObject
 {
-    public string Name;
+    [SerializeField] private string _id;
+    [SerializeField] private string _name;
+
+    public string Id => _id;
+    public string Name => _name;
 }
