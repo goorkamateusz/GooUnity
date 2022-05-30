47d96f2a3d1d278dbaa0a1d8e0aab48aae1e3263
Górka Mateusz <maatiug@gmail.com>
2022-05-31 01:17:53 +0200

    Fixed references to build

---
+++ Packages/com.goorkamateusz.characters/Tests/Editor/ExampleTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+namespace Goo.Characters.Test.Editor
+{
+    public class ExampleTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+        }
+    }
+}
